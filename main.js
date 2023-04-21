abfa8fd8f2e65db5303c122376417595ef1aef56
Dylan Foley <dylan@Dylans-MBP.attlocal.net>
2023-04-21 17:57:17 -0400

    added user input functionality and chunking of prompts

--- main.js
+++ main.js
@@ -2,27 +2,70 @@ import * as dotenv from "dotenv";
 dotenv.config();
 import { ChatGPTAPI } from "chatgpt";
 import wtf from "wtf_wikipedia";
+import readline from "readline";
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const api = new ChatGPTAPI({
+  apiKey: process.env.OPENAI_API_KEY,
+  systemMessage:
+    "Summarize the following content while never saying things such as the article, or this passage, etc. also if there are follow up questions being asked, you need to make sure you are not going to repeat any informatoin unless it is critical for the summary",
+  completionParams: {
+    model: "gpt-3.5-turbo-0301",
+  },
+});
+
+let contentChunks = [];
+let currentChunk = "";
+
+function addContent({ sentences }) {
+  const content = sentences?.map((sentence) => sentence.text).join(" ");
+  currentChunk += content;
+}
 
 function parseSection(section) {
   const sectionTitle = section.title || "Intro";
   if (["See also", "Notes", "External links"].includes(sectionTitle)) {
-    return;
+    return "";
   }
-  console.log(`\nSection: ${sectionTitle}`);
   section.paragraphs?.forEach((paragraph) => {
-    logParagraph(paragraph);
+    addContent(paragraph);
   });
-}
-
-function logParagraph({ sentences }) {
-  const content = sentences?.map((sentence) => sentence.text).join(" ");
-  console.log(content);
+  if (currentChunk.length > 3000) {
+    contentChunks.push(currentChunk);
+    currentChunk = "";
+  }
 }
 
 async function parseDoc() {
-  let doc = await wtf.fetch("Mill Creek Park");
-  const docJSON = doc.json();
-  docJSON.sections.forEach(parseSection);
+  rl.question(
+    "Enter a Wikipedia article title: ",
+    async function (articleTitle) {
+      if (!articleTitle) {
+        throw new Error("Please enter a valid Wikipedia article title");
+      }
+      let doc = await wtf.fetch(articleTitle);
+      if (!doc) {
+        throw new Error("Could not find article with that title");
+      }
+      const docJSON = doc.json();
+      docJSON.sections.forEach(parseSection);
+      if (currentChunk.length > 0) {
+        contentChunks.push(currentChunk);
+      }
+      rl.close();
+      const summaries = await Promise.all(
+        contentChunks.map(async (chunk) => {
+          const res = await api.sendMessage(chunk);
+          return res.text;
+        })
+      );
+      console.log(summaries.join("\n"));
+    }
+  );
 }
 
 parseDoc().catch((err) => {
